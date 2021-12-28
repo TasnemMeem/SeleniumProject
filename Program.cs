8bca311bde8236ff0e89eb23c387d9d6ccc12c6f
kumol <kumol@maptaskr.com>
2021-12-28 15:00:12 +0600

    Add all my files

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+
+namespace SeleniumProjectOPUS
+{
+    class Program
+    {
+        public static string webUrl = "https://bongobd.com/"; 
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(webUrl);
+            driver.Manage().Window.Maximize();
+
+            PlayVideo(driver);
+            Console.WriteLine("Video Play Successfully");
+        }
+
+
+        public static void PlayVideo(IWebDriver driver)
+        {
+            //Search Box Clear
+            driver.FindElement(By.XPath("//*[@id='root']/ div/div/div/div[2]/header/div/div/div[2]/div[2]/div[1]/div[2]/input")).Clear();
+            Thread.Sleep(5000);
+
+            //Search Box Input Value
+            driver.FindElement(By.XPath("//*[@id='root']/ div/div/div/div[2]/header/div/div/div[2]/div[2]/div[1]/div[2]/input")).SendKeys("Panther");
+            Thread.Sleep(5000);
+
+            //Search Box Input Enter Press
+            driver.FindElement(By.XPath("//*[@id='root']/ div/div/div/div[2]/header/div/div/div[2]/div[2]/div[1]/div[2]/input")).SendKeys(Keys.Return);
+            Thread.Sleep(5000);
+
+            //Click Movie List Image
+            driver.FindElement(By.XPath("//*[@id='root']/div/div/div/div[3]/div/div/div/div[1]/div/div[2]/div/a/div/div/div/div[1]/div/img")).Click();
+            
+            Thread.Sleep(5000);
+        }
+    }
+}
